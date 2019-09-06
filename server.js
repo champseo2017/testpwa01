bd3eacdc830e31db6ea224a0b56b41a3ec922902
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-07 03:39:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require('url')
 const next = require('next');
 const cache = require('lru-cache'); // for using least-recently-used based caching
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
