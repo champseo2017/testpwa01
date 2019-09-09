a0da69b0eb56e8994f8621484b2b48a310ef324c
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-10 01:43:23 +0700

    update

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Head from 'next/head';
 import Header from './Header/Header'
 import 'bootstrap/dist/css/bootstrap.css';
-import '../style.css'
 
 const Layout = (props) => {
     
