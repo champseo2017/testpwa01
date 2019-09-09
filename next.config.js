1f40e7acf7bbcbb52077dc9c563d092a0a924d81
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-10 02:24:41 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = withOffline(withCSS({
       fs: 'empty',
     };
 
-    if (!isServer) {
+    if (isServer) {
       config.module.rules.find(({ test }) => test.test('style.css')).use.push({
         loader: 'css-purify-webpack-loader',
         options: {
@@ -57,7 +57,7 @@ module.exports = withOffline(withCSS({
     };
 
 
-    if (!isServer && !dev) {
+    if (isServer) {
       config.plugins.push(
         new NextWorkboxPlugin({
           buildId,
